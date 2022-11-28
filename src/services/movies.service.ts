dbac3303f10af0e58b2d8776151ffd50ae0d8c18
thadek <gabipamic@gmail.com>
2022-11-28 15:51:34 -0300

    some changes

--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -41,6 +41,7 @@ export const getMovie = async (id?: string) => {
     }
 }
 
+
 export const searchMovies = async (query: string, page?: number) => {
     try {
         const response = await axios({
